9f6e46981afbc7e5855295f765053bb8ba399ba6
Côme Chilliet <come@opensides.be>
2017-08-30 15:58:18 +0200

    Removed problematic comma

--- redmine_gitlab_migrator/sql.py
+++ redmine_gitlab_migrator/sql.py
@@ -29,7 +29,7 @@ WHERE title ~* '{regex}' AND project_id={project_id};
 
 SET_USER_CREATION_DATE = r"""
 UPDATE users SET
-  created_at = '{date}',
+  created_at = '{date}'
 WHERE username='{login}' AND project_id='{project_id}';
 """
 
